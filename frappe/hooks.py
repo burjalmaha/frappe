674d3819bf938c60157efaf656fcda5be19aa129
m4mousa <m.mousa@burjalmaha.com>
2025-03-17 08:17:53 +0000

    fix: disable all daily hooks

--- frappe/hooks.py
+++ frappe/hooks.py
@@ -214,29 +214,29 @@ scheduler_events = {
 		"frappe.email.doctype.newsletter.newsletter.send_scheduled_email"
 	],
 	"daily": [
-		"frappe.email.queue.set_expiry_for_email_queue",
-		"frappe.desk.notifications.clear_notifications",
-		"frappe.core.doctype.error_log.error_log.set_old_logs_as_seen",
-		"frappe.desk.doctype.event.event.send_event_digest",
+		# "frappe.email.queue.set_expiry_for_email_queue",
+		# "frappe.desk.notifications.clear_notifications",
+		# "frappe.core.doctype.error_log.error_log.set_old_logs_as_seen",
+		# "frappe.desk.doctype.event.event.send_event_digest",
 		"frappe.sessions.clear_expired_sessions",
-		"frappe.email.doctype.notification.notification.trigger_daily_alerts",
-		"frappe.utils.scheduler.restrict_scheduler_events_if_dormant",
-		"frappe.email.doctype.auto_email_report.auto_email_report.send_daily",
-		"frappe.website.doctype.personal_data_deletion_request.personal_data_deletion_request.remove_unverified_record",
-		"frappe.desk.form.document_follow.send_daily_updates",
-		"frappe.social.doctype.energy_point_settings.energy_point_settings.allocate_review_points",
-		"frappe.integrations.doctype.google_contacts.google_contacts.sync",
-		"frappe.automation.doctype.auto_repeat.auto_repeat.make_auto_repeat_entry",
-		"frappe.automation.doctype.auto_repeat.auto_repeat.set_auto_repeat_as_completed",
-		"frappe.email.doctype.unhandled_email.unhandled_email.remove_old_unhandled_emails",
-		"frappe.core.doctype.prepared_report.prepared_report.delete_expired_prepared_reports",
-		"frappe.core.doctype.log_settings.log_settings.run_log_clean_up"
+		# "frappe.email.doctype.notification.notification.trigger_daily_alerts",
+		"frappe.utils.scheduler.restrict_scheduler_events_if_dormant"
+		# "frappe.email.doctype.auto_email_report.auto_email_report.send_daily",
+		# "frappe.website.doctype.personal_data_deletion_request.personal_data_deletion_request.remove_unverified_record",
+		# "frappe.desk.form.document_follow.send_daily_updates",
+		# "frappe.social.doctype.energy_point_settings.energy_point_settings.allocate_review_points",
+		# "frappe.integrations.doctype.google_contacts.google_contacts.sync",
+		# "frappe.automation.doctype.auto_repeat.auto_repeat.make_auto_repeat_entry",
+		# "frappe.automation.doctype.auto_repeat.auto_repeat.set_auto_repeat_as_completed",
+		# "frappe.email.doctype.unhandled_email.unhandled_email.remove_old_unhandled_emails",
+		# "frappe.core.doctype.prepared_report.prepared_report.delete_expired_prepared_reports",
+		# "frappe.core.doctype.log_settings.log_settings.run_log_clean_up"
 	],
 	"daily_long": [
-		"frappe.integrations.doctype.dropbox_settings.dropbox_settings.take_backups_daily",
-		"frappe.utils.change_log.check_for_update",
-		"frappe.integrations.doctype.s3_backup_settings.s3_backup_settings.take_backups_daily",
-		"frappe.integrations.doctype.google_drive.google_drive.daily_backup"
+		# "frappe.integrations.doctype.dropbox_settings.dropbox_settings.take_backups_daily",
+		# "frappe.utils.change_log.check_for_update",
+		# "frappe.integrations.doctype.s3_backup_settings.s3_backup_settings.take_backups_daily",
+		# "frappe.integrations.doctype.google_drive.google_drive.daily_backup"
 	],
 	"weekly_long": [
 		"frappe.integrations.doctype.dropbox_settings.dropbox_settings.take_backups_weekly",
